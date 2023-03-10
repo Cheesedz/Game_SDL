b9361b68d92e212edee8ecd014e5163dfa1a6286
Cheesedz <113958111+Cheesedz@users.noreply.github.com>
2023-03-10 21:19:29 +0700

    Add files via upload

---
+++ BaseObject.h
@@ -0,0 +1,39 @@
+﻿#pragma once
+
+#include "CommonFunction.h"
+
+//Lớp chuyên xử lí hình ảnh
+class BaseObject
+{
+public:
+	//Constructor
+	BaseObject();
+
+	//Destructor
+	~BaseObject();
+
+	//Khởi tạo thông số cho rect
+	void SetRect(const int& x, const int& y) {
+		rect_.x = x;
+		rect_.y = y;
+	}
+
+	//Xuất ra rect
+	SDL_Rect GetRect() const { return rect_;}
+
+	//Xuất p_object_
+	SDL_Texture* GetObject() const { return p_object_; }
+
+	//Load ảnh bitmap lên p_object_
+	bool LoadImage(std::string path, SDL_Renderer* screen);
+
+	void Render(SDL_Renderer* des, const SDL_Rect* clip = NULL);
+	void Free();
+
+protected:
+	//Lưu trữ hình ảnh
+	SDL_Texture* p_object_;
+
+	//Lưu trữ kích thước
+	SDL_Rect rect_;
+};
---
+++ CommonFunction.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+//Các thư viện thường dùng để xử lí game
+#include <Windows.h>
+#include <string>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <SDL.h>
+#include <SDL_ttf.h>
+
+//Cửa sổ game
+static SDL_Window* gWindow = NULL;
+
+//Render game
+static SDL_Renderer* gScreen = NULL;
+
+//Sự kiện từ bàn phím, chuột
+static SDL_Event* gEvent;
+
+//Các thông số màn hình game
+const int SCREEN_HEIGHT = 640;
+const int SCREEN_WIDTH = 1280;
+
+//Thông số hình ảnh game(pixel)
+const int SCREEN_BPP = 32;
+
+//Thông số màu sắc transparent
+const int COLOR_KEY_R = 167;
+const int COLOR_KEY_G = 175;
+const int COLOR_KEY_B = 180;
