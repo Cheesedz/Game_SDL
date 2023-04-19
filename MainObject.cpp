539c51626d1be4248aff48c14d35eec71c698bb6
Cheesedz <113958111+Cheesedz@users.noreply.github.com>
2023-04-19 12:37:12 +0700

    Update MainObject.cpp

--- MainObject.cpp
+++ MainObject.cpp
@@ -1,9 +1,14 @@
 #include "MainObject.h"
+#include "Timer.h"
 
 #define GRAVITY 1.2
 #define MAX_FALL_SPEED 10
-#define PLAYER_RUN_SPEED 5
+#define PLAYER_RUN_SPEED 15
 #define PLAYER_JUMP_SPEED 15
+#define HEART 3
+#define ENERGY 2
+#define SPIKE 4
+#define SPIKE_INVERSE 99
 
 MainObject::MainObject()
 {
@@ -25,9 +30,15 @@ MainObject::MainObject()
 	respawnTime = 0;
 }
 
+int cooldown = 0;
+
 MainObject :: ~MainObject()
 {
 
+}
+void MainObject::increaseEnergy()
+{
+	energy_count++;
 }
 
 bool MainObject::LoadImg(std::string path, SDL_Renderer* screen)
@@ -42,6 +53,16 @@ bool MainObject::LoadImg(std::string path, SDL_Renderer* screen)
 	return ret;
 }
 
+SDL_Rect MainObject::getRectFrame()
+{
+	SDL_Rect gRect;
+	gRect.x = rect.x;
+	gRect.y = rect.y;
+	gRect.w = width_frame;
+	gRect.h = height_frame;
+	return gRect;
+}
+
 void MainObject::SetClips()
 {
 	if (width_frame > 0 && height_frame > 0)
@@ -54,32 +75,37 @@ void MainObject::SetClips()
 		frame_clip[1].x = width_frame;
 		frame_clip[1].y = 0;
 		frame_clip[1].w = width_frame;
-		frame_clip[1].h = width_frame;
+		frame_clip[1].h = height_frame;
 
 		frame_clip[2].x = 2*width_frame;
 		frame_clip[2].y = 0;
 		frame_clip[2].w = width_frame;
-		frame_clip[2].h = width_frame;
+		frame_clip[2].h = height_frame;
 
 		frame_clip[3].x = 3*width_frame;
 		frame_clip[3].y = 0;
 		frame_clip[3].w = width_frame;
-		frame_clip[3].h = width_frame;
+		frame_clip[3].h = height_frame;
 
-		frame_clip[4].x = width_frame;
+		frame_clip[4].x = 4*width_frame;
 		frame_clip[4].y = 0;
 		frame_clip[4].w = width_frame;
-		frame_clip[4].h = width_frame;
+		frame_clip[4].h = height_frame;
 
-		frame_clip[5].x = width_frame;
+		frame_clip[5].x = 5*width_frame;
 		frame_clip[5].y = 0;
 		frame_clip[5].w = width_frame;
-		frame_clip[5].h = width_frame;
+		frame_clip[5].h = height_frame;
 
-		frame_clip[6].x = width_frame;
+		frame_clip[6].x =6* width_frame;
 		frame_clip[6].y = 0;
 		frame_clip[6].w = width_frame;
-		frame_clip[6].h = width_frame;
+		frame_clip[6].h = height_frame;
+
+		frame_clip[7].x = 7 * width_frame;
+		frame_clip[7].y = 0;
+		frame_clip[7].w = width_frame;
+		frame_clip[7].h = height_frame;
 	}
 }
 
@@ -93,10 +119,14 @@ void MainObject::Show(SDL_Renderer* des)
 	{
 		LoadImg("res//megaman_right.png", des);
 	}
-	else if (status == IDLE)
+	/*else if (status == JUMP)
 	{
-		LoadImg("res//megaman_idle.png", des);
+		LoadImg("res//Jump.png", des);
 	}
+	else if (status == FALL)
+	{
+		LoadImg("res//Fall.png", des);
+	}*/
 
 	if (input_type.left == 1 || input_type.right == 1)
 	{
@@ -107,7 +137,7 @@ void MainObject::Show(SDL_Renderer* des)
 		frame = 0;
 	}
 
-	if (frame >= 5) frame = 0;
+	if (frame >= 7) frame = 0;
 	rect.x = x_pos - map_x;
 	rect.y = y_pos - map_y;
 
@@ -130,7 +160,7 @@ void MainObject::HandleInputEvent(SDL_Event events, SDL_Renderer* screen)
 			status = WALK_RIGHT;
 			input_type.right = 1;
 			input_type.left = 0;
-			input_type.up = 0;
+			input_type.jump = 0;
 			input_type.down = 0;
 		}
 		break;
@@ -140,38 +170,50 @@ void MainObject::HandleInputEvent(SDL_Event events, SDL_Renderer* screen)
 			status = WALK_LEFT;
 			input_type.left = 1;
 			input_type.right = 0;
-			input_type.up = 0;
+			input_type.jump = 0;
 			input_type.down = 0;
 		}
 		break;
 
 		case SDLK_w:
 		{
+			/*status = JUMP;*/
+			/*input_type.down = 0;*/
 			input_type.jump = 1;
+			
 		}
 		break;
-
+		
 		case SDLK_j:
 		{
-			Bullet* bullet = new Bullet();
-
-			if (status == WALK_LEFT)
+			cooldown++;
+			if (cooldown > 3) cooldown = 0;
+			if (cooldown == 0)
 			{
-				bullet->LoadImg("res//bullet_left.png", screen);
-				bullet->setBulletDirect(Bullet::DIR_LEFT);
-				bullet->SetRect(this->rect.x, rect.y + height_frame * 0.2);
-			}
-			else
-			{
-				bullet->LoadImg("res//bullet_right.png", screen);
-				bullet->setBulletDirect(Bullet::DIR_RIGHT);
-				bullet->SetRect(this->rect.x + width_frame - 20, rect.y + height_frame * 0.2);
-			}
-			bullet->setX(20);
-			bullet->setIsMove(true);
+				Bullet* bullet = new Bullet();
 
-			bullet_list.push_back(bullet);
+				if (status == WALK_LEFT)
+				{
+					bullet->LoadImg("res//blade_left.png", screen);
+					bullet->setBulletDirect(Bullet::DIR_LEFT);
+					bullet->SetRect(this->rect.x, rect.y + height_frame * 0.2);
+				}
+				else
+				{
+					bullet->LoadImg("res//blade_right.png", screen);
+					bullet->setBulletDirect(Bullet::DIR_RIGHT);
+					bullet->SetRect(this->rect.x + width_frame - 20, rect.y + height_frame * 0.2);
+				}
+				bullet->setX(20);
+				bullet->setIsMove(true);
+
+				bullet_list.push_back(bullet);
+
+			}
+			
+
 		}
+		
 		break;
 
 		default:
@@ -185,13 +227,13 @@ void MainObject::HandleInputEvent(SDL_Event events, SDL_Renderer* screen)
 		{
 		case SDLK_d:
 		{
-			status = WALK_RIGHT ;
+			status = WALK_RIGHT;
 			input_type.right = 0;
 		}
 		break;
 		case SDLK_a:
 		{
-			status = WALK_LEFT ;
+			status = WALK_LEFT;
 			input_type.left = 0;
 		}
 		break;
@@ -228,6 +270,22 @@ void MainObject::HandleBullet(SDL_Renderer* des)
 					bullet = NULL;
 				}
 			}
+		}
+	}
+}
+
+void MainObject::RemoveBullet(const int& index)
+{
+	int size = bullet_list.size();
+	if (size > 0 && index < size)
+	{
+		Bullet* p_bullet = bullet_list.at(index);
+		bullet_list.erase(bullet_list.begin() + index);
+
+		if (p_bullet)
+		{
+			delete p_bullet;
+			p_bullet = NULL;
 		}
 	}
 }
@@ -334,7 +392,23 @@ void MainObject::checkMap(Map& mapData)
 	{
 		if (x_val > 0) //main opbject is moving to right
 		{
-			if (mapData.tile[y1][x2] != BLANK_TILE || mapData.tile[y2][x2] != BLANK_TILE)
+			int val1 = mapData.tile[y1][x2];
+			int val2 = mapData.tile[y2][x2];
+
+			if (val1 == HEART || val2 == HEART)
+			{
+				mapData.tile[y1][x2] = 0;
+				mapData.tile[y2][x2] = 0;
+			}
+
+			if (val1 == ENERGY || val2 == ENERGY)
+			{
+				mapData.tile[y1][x2] = 0;
+				mapData.tile[y2][x2] = 0;
+				increaseEnergy();
+			}
+
+			if ((val1 != BLANK_TILE && val1 != SPIKE && val1 != SPIKE_INVERSE)|| (val2 != BLANK_TILE && val2 != SPIKE && val2 != SPIKE_INVERSE ))
 			{
 				x_pos = x2 * TILE_SIZE;
 				x_pos -= width_frame + 1;
@@ -343,7 +417,23 @@ void MainObject::checkMap(Map& mapData)
 		}
 		else if (x_val < 0)
 		{
-			if (mapData.tile[y1][x1] != BLANK_TILE || mapData.tile[y2][x1] != BLANK_TILE)
+			int val1 = mapData.tile[y1][x1];
+			int val2 = mapData.tile[y2][x1];
+
+			if (val1 == HEART || val2 == HEART)
+			{
+				mapData.tile[y1][x1] = 0;
+				mapData.tile[y2][x1] = 0;
+			}
+
+			if (val1 == ENERGY || val2 == ENERGY)
+			{
+				mapData.tile[y1][x2] = 0;
+				mapData.tile[y2][x2] = 0;
+				increaseEnergy();
+			}
+
+			if ((val1 != BLANK_TILE && val1 != SPIKE && val1 != SPIKE_INVERSE ) || (val2 != BLANK_TILE && val2 != SPIKE && val2 != SPIKE_INVERSE ))
 			{
 				x_pos = (x1 + 1) * TILE_SIZE;
 				x_val = 0;
@@ -363,7 +453,23 @@ void MainObject::checkMap(Map& mapData)
 	{
 		if (y_val > 0)
 		{
-			if (mapData.tile[y2][x1] != BLANK_TILE || mapData.tile[y2][x2] != BLANK_TILE)
+			int val1 = mapData.tile[y2][x1];
+			int val2 = mapData.tile[y2][x2];
+
+			if (val1 == HEART || val2 == HEART)
+			{
+				mapData.tile[y2][x1] = 0;
+				mapData.tile[y2][x2] = 0;
+			}
+
+			if (val1 == ENERGY || val2 == ENERGY)
+			{
+				mapData.tile[y1][x2] = 0;
+				mapData.tile[y2][x2] = 0;
+				increaseEnergy();
+			}
+
+			if ((val1 != BLANK_TILE && val1 != SPIKE && val1 != SPIKE_INVERSE ) || (val2 != BLANK_TILE && val2 != SPIKE && val2 != SPIKE_INVERSE ))
 			{
 				y_pos = y2 * TILE_SIZE;
 				y_pos -= (height_frame + 1);
@@ -375,7 +481,24 @@ void MainObject::checkMap(Map& mapData)
 		}
 		else if (y_val < 0)
 		{
-			if (mapData.tile[y1][x1] != BLANK_TILE || mapData.tile[y1][x2] != BLANK_TILE)
+
+			int val1 = mapData.tile[y1][x1];
+			int val2 = mapData.tile[y1][x2];
+
+			if (val1 == HEART || val2 == HEART)
+			{
+				mapData.tile[y1][x1] = 0;
+				mapData.tile[y1][x2] = 0;
+			}
+
+			if (val1 == ENERGY || val2 == ENERGY)
+			{
+				mapData.tile[y1][x2] = 0;
+				mapData.tile[y2][x2] = 0;
+				increaseEnergy();
+			}
+
+			if ((val1 != BLANK_TILE && val1 != SPIKE && val1 != SPIKE_INVERSE ) || (val2 != BLANK_TILE && val2 != SPIKE && val2 != SPIKE_INVERSE ))
 			{
 				y_pos = (y1 + 1) * TILE_SIZE;
 				y_val = 0;
